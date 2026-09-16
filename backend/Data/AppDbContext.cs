dc9531be9098f408e11435ab451f0fb337b41ee8
PU-MEriksson <malin.eriksson83@icloud.com>
2026-09-16 13:58:14 +0200

    Add Entity Framework Core support with SQLite and initial migration for Book model

---
+++ backend/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+using Books.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Books.Api.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Book> Books => Set<Book>();
+}
