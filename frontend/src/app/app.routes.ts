dc954a66a00962f31ebee965e6c52bc122ebcae9
PU-MEriksson <malin.eriksson83@icloud.com>
2026-09-23 11:06:34 +0200

    Add login component with form and styles

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { BookList } from './pages/book-list/book-list';
 import { BookForm } from './pages/book-form/book-form';
+import { Login } from './pages/login/login';
 
 export const routes: Routes = [
   { path: '', component: BookList },
+  { path: 'login', component: Login },
   { path: 'books/new', component: BookForm },
   { path: 'books/edit/:id', component: BookForm },
 ];
---
+++ frontend/src/app/pages/login/login.ts
@@ -0,0 +1,42 @@
+import { Component, inject, signal } from '@angular/core';
+import { HttpErrorResponse, HttpResponse } from '@angular/common/http';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth-service';
+
+@Component({
+  selector: 'app-login',
+  imports: [ReactiveFormsModule],
+  templateUrl: './login.html',
+  styleUrl: './login.css',
+})
+export class Login {
+  private authService = inject(AuthService);
+  private router = inject(Router);
+  private fb = inject(FormBuilder);
+
+  protected errorMessage = signal<string | null>(null);
+
+  protected form = this.fb.nonNullable.group({
+    username: ['', Validators.required],
+    password: ['', Validators.required],
+  });
+
+  protected onSubmit() {
+    if (this.form.invalid) {
+      return;
+    }
+
+    this.errorMessage.set(null);
+
+    this.authService.login(this.form.getRawValue()).subscribe({
+      next: () => this.router.navigate(['/']),
+      error: (error: HttpErrorResponse) =>
+        this.errorMessage.set(
+          error.status === 401
+            ? 'Fel användarnamn eller lösenord.'
+            : 'Något gick fel. Försök igen om en stund',
+        ),
+    });
+  }
+}
