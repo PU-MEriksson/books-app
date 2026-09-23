7bab15ed044c499829189210710e9624469c71c5
PU-MEriksson <malin.eriksson83@icloud.com>
2026-09-23 11:38:01 +0200

    Add auth guard and update routes to protect book management and home pages

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,10 +2,11 @@ import { Routes } from '@angular/router';
 import { BookList } from './pages/book-list/book-list';
 import { BookForm } from './pages/book-form/book-form';
 import { Login } from './pages/login/login';
+import { authGuard } from './guards/auth-guard';
 
 export const routes: Routes = [
-  { path: '', component: BookList },
+  { path: '', component: BookList, canActivate: [authGuard] },
   { path: 'login', component: Login },
-  { path: 'books/new', component: BookForm },
-  { path: 'books/edit/:id', component: BookForm },
+  { path: 'books/new', component: BookForm, canActivate: [authGuard] },
+  { path: 'books/edit/:id', component: BookForm, canActivate: [authGuard] },
 ];
