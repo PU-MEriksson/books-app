961bee9fd5187505824675bbe689b53613ec7d7a
PU-MEriksson <malin.eriksson83@icloud.com>
2026-09-25 14:29:33 +0200

    Enhance book card component: format publication date, improve button styles, and add DatePipe for date formatting

--- frontend/src/app/components/book-card/book-card.ts
+++ frontend/src/app/components/book-card/book-card.ts
@@ -1,10 +1,11 @@
 import { Component, input, output } from '@angular/core';
 import { Book } from '../../models/book';
 import { RouterLink } from '@angular/router';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-book-card',
-  imports: [RouterLink],
+  imports: [RouterLink, DatePipe],
   templateUrl: './book-card.html',
   styleUrl: './book-card.css',
 })
